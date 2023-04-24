2c7e8af0b08acdb0ae7492ac8afa9238fac46c94
kiyotd <torigoedesign@gmail.com>
2023-04-25 01:35:45 +0900

    feat(utils): add YouTube class to check if a URL is a valid YouTube video URL test(utils): add unit tests for YouTube class to check if a URL is a valid YouTube video URL

---
+++ src/sns/YouTube.php
@@ -0,0 +1,19 @@
+<?php
+
+  namespace kiyotd\utils\sns;
+
+  class YouTube
+  {
+    /**
+     * Returns a boolean value if the URL is a YouTube video or not.
+     *
+     * @param string $url
+     * @return bool
+     */
+    public static function is_youtube_url(string $url): bool
+    {
+      $pattern = '/^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/watch\?v=[\w-]{11}$/';
+
+      return (bool)preg_match($pattern, $url);
+    }
+  }
---
+++ tests/YouTubeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+  use kiyotd\utils\sns\YouTube;
+  use PHPUnit\Framework\TestCase;
+
+  class YouTubeTest extends TestCase
+  {
+    public function testIsYoutubeUrlReturnsTrueForValidUrl()
+    {
+      $validUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+      $result = YouTube::is_youtube_url($validUrl);
+
+      $this->assertTrue($result);
+    }
+
+    public function testIsYoutubeUrlReturnsFalseForInvalidUrl()
+    {
+      $invalidUrl = 'https://www.google.com/';
+      $result = YouTube::is_youtube_url($invalidUrl);
+
+      $this->assertFalse($result);
+    }
+
+    public function testIsYoutubeUrlReturnsFalseForShortenedUrl()
+    {
+      $shortenedUrl = 'https://youtu.be/dQw4w9WgXcq';
+      $result = YouTube::is_youtube_url($shortenedUrl);
+
+      $this->assertFalse($result);
+    }
+
+    public function testIsYoutubeUrlReturnsFalseForInvalidVideoId()
+    {
+      $invalidVideoIdUrl = 'https://www.youtube.com/watch?v=invalid-id';
+      $result = YouTube::is_youtube_url($invalidVideoIdUrl);
+
+      $this->assertFalse($result);
+    }
+  }
