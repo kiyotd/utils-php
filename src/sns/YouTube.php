3911de103979adb1699614b95d470b8d0e2fbd03
kiyotd <torigoedesign@gmail.com>
2023-10-25 05:00:42 +0900

    🐛 fix(YouTube.php): fix indentation and namespace declaration in YouTube class 🐛 fix(YouTube.php): fix indentation and add missing whitespace in is_youtube_url method ✅ test(YouTubeTest.php): add unit tests for is_youtube_url method in YouTube class

--- src/sns/YouTube.php
+++ src/sns/YouTube.php
@@ -1,9 +1,9 @@
 <?php
 
-  namespace kiyotd\utils\sns;
+namespace kiyotd\utils\sns;
 
-  class YouTube
-  {
+class YouTube
+{
     /**
      * Returns a boolean value if the URL is a YouTube video or not.
      *
@@ -12,8 +12,8 @@
      */
     public static function is_youtube_url(string $url): bool
     {
-      $pattern = '/^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/watch\?v=[\w-]{11}$/';
+        $pattern = '/^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/watch\?v=[\w-]{11}$/';
 
-      return (bool)preg_match($pattern, $url);
+        return (bool)preg_match($pattern, $url);
     }
-  }
+}
--- tests/YouTubeTest.php
+++ tests/YouTubeTest.php
@@ -1,39 +1,39 @@
 <?php
 
-  use kiyotd\utils\sns\YouTube;
-  use PHPUnit\Framework\TestCase;
+use kiyotd\utils\sns\YouTube;
+use PHPUnit\Framework\TestCase;
 
-  class YouTubeTest extends TestCase
-  {
+class YouTubeTest extends TestCase
+{
     public function testIsYoutubeUrlReturnsTrueForValidUrl()
     {
-      $validUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-      $result = YouTube::is_youtube_url($validUrl);
+        $validUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+        $result = YouTube::is_youtube_url($validUrl);
 
-      $this->assertTrue($result);
+        $this->assertTrue($result);
     }
 
     public function testIsYoutubeUrlReturnsFalseForInvalidUrl()
     {
-      $invalidUrl = 'https://www.google.com/';
-      $result = YouTube::is_youtube_url($invalidUrl);
+        $invalidUrl = 'https://www.google.com/';
+        $result = YouTube::is_youtube_url($invalidUrl);
 
-      $this->assertFalse($result);
+        $this->assertFalse($result);
     }
 
     public function testIsYoutubeUrlReturnsFalseForShortenedUrl()
     {
-      $shortenedUrl = 'https://youtu.be/dQw4w9WgXcq';
-      $result = YouTube::is_youtube_url($shortenedUrl);
+        $shortenedUrl = 'https://youtu.be/dQw4w9WgXcq';
+        $result = YouTube::is_youtube_url($shortenedUrl);
 
-      $this->assertFalse($result);
+        $this->assertFalse($result);
     }
 
     public function testIsYoutubeUrlReturnsFalseForInvalidVideoId()
     {
-      $invalidVideoIdUrl = 'https://www.youtube.com/watch?v=invalid-id';
-      $result = YouTube::is_youtube_url($invalidVideoIdUrl);
+        $invalidVideoIdUrl = 'https://www.youtube.com/watch?v=invalid-id';
+        $result = YouTube::is_youtube_url($invalidVideoIdUrl);
 
-      $this->assertFalse($result);
+        $this->assertFalse($result);
     }
-  }
+}
